cd25ac14e2a3c8cd186c682f4adc3c6759d922fa
Iuoras Razvan <iuorasrazvan2003@yahoo.com>
2016-05-05 15:10:30 +0300

    Commit 2 version v2.0.0
    Modified index.js and package.json with new version.

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var lodash = require('lodash');
  
 var output = lodash.without([1, 2, 3], 1);
 console.log(output);
+
+exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+}
